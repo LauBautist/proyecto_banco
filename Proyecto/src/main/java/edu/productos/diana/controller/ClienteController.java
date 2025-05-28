0d6033d744be935ea52eb665149d05ada3f8e9be
LauBautist <dianalaurabau12345@gmail.com>
2025-05-28 16:38:52 -0600

    ClienteController.java

--- Proyecto/src/main/java/edu/productos/diana/controller/ClienteController.java
+++ Proyecto/src/main/java/edu/productos/diana/controller/ClienteController.java
@@ -75,7 +75,7 @@ public class ClienteController {
 
 		// Subida de imagen
 		if (!file.isEmpty()) {
-			String rutaDirectorio = "imagenesProyecto/";
+			String rutaDirectorio = System.getProperty("user.dir") + "/imagenesProyecto/";
 			String nombreArchivo = file.getOriginalFilename();
 			File archivoDestino = new File(rutaDirectorio + nombreArchivo);
 
